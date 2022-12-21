6af3f555d98693be4831b6e3833e5a33537432c8
Florin Balate <florin.balate@gmail.com>
2022-12-21 20:29:46 +0200

    customize lualine

--- after/plugin/defaults.lua
+++ after/plugin/defaults.lua
@@ -1,9 +1,22 @@
+vim.opt.backspace = '2'
+vim.opt.showcmd = true
+vim.opt.laststatus = 2
+vim.opt.autowrite = true
+vim.opt.cursorline = true
+vim.opt.autoread = true
+
+-- use spaces for tabs and more
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.shiftround = true
+vim.opt.expandtab = true
+vim.opt.relativenumber = true
+
 -- gruvbox
 vim.o.termguicolors = true
 vim.cmd [[ colorscheme gruvbox ]]
 
 -- custom search resume
-vim.opt.relativenumber = true
 vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = '[S]earch [R]esume' })
 
 -- nvim-tree
@@ -11,3 +24,19 @@ vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
 vim.keymap.set('n', '<c-n>', ':NvimTreeFindFileToggle<CR>')
+
+-- lualine options
+require('lualine').setup({
+	options = {
+		icons_enabled = true,
+		theme = 'gruvbox',
+	},
+	sections = {
+		lualine_a = {
+			{
+				'filename',
+				path = 1,
+			}
+		}
+	}
+})
