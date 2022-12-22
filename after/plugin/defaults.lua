f1e6f14f30da8d288621dc995a4a946e0a343ec9
Florin Balate <florin.balate@gmail.com>
2022-12-22 13:37:04 +0200

    added dap and neotest

--- after/plugin/defaults.lua
+++ after/plugin/defaults.lua
@@ -40,3 +40,28 @@ require('lualine').setup({
 		}
 	}
 })
+
+local dapui =require('dapui')
+local dap = require('dap')
+dap.listeners.after.event_initialized["dapui_config"] = function ()
+	dapui.open()
+end
+dap.listeners.before.event_terminated["dapui_config"] = function ()
+	dapui.close()
+end
+dap.listeners.before.event_exited["dapui_config"]	= function ()
+	dapui.close()
+end
+
+-- require("dap.ext.vscode").load_launchjs()
+local continue = function()
+  if vim.fn.filereadable('.vscode/launch.json') then
+    require('dap.ext.vscode').load_launchjs(nil, {})
+  end
+  require('dap').continue()
+end
+vim.keymap.set('n', '<leader>dm', require('dap-python').test_method, { desc = '[D]ebug [M]ethod'})
+vim.keymap.set('n', '<leader>dc', continue, { desc = '[D]ebug Start/[C]ontinue'})
+vim.keymap.set('n', '<leader>dt', require('dap').toggle_breakpoint, { desc = '[D]ebug [T]oggle breakpoint'})
+vim.keymap.set('n', '<leader>di', require('dap').step_into, { desc = '[D]ebug Step [I]nto'})
+vim.keymap.set('n', '<leader>do', require('dap').step_over, { desc = '[D]ebug Steo [O]ver'})
