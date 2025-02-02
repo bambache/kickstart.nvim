45729d156d7fdf5784e2cc9f5779f0665a1fdc0b
Florin Balate <florin.balate@upandrunningsoftware.com>
2025-02-02 13:49:32 +0200

    * add oatmeal plugin

--- lua/custom/plugins/plugins.lua
+++ lua/custom/plugins/plugins.lua
@@ -135,4 +135,15 @@ return {
       })
     end
   }),
+  ({
+    "dustinblackman/oatmeal.nvim",
+    cmd = { "Oatmeal" },
+    keys = {
+      { "<leader>om", mode = "n", desc = "Start Oatmeal session" },
+    },
+    opts = {
+      backend = "ollama",
+      model = "codellama:latest",
+    },
+  }),
 }
